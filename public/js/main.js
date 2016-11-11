11c75c50c5c12b6305156bc6195713d75b84ff64
Prateek Madapurmath <prateekm33@gmail.com>
2016-11-10 17:46:06 -0800

    (cleanup) cleans up code and lays part of foundation for encryption service

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
-  const $rsaForm = $('#rsa-form')
+  initRSAkeyUI();
+  initFileEncryptionService();
+});
 
+
+/* -------- RSA Key Gen Service --------- */
+
+function initRSAkeyUI() {
+  const $rsaForm = $('#rsa-form')
   $rsaForm.on('submit', evt => { 
     const passphrase = $('#passphrase').val();
     $('#passphrase').val('')
@@ -15,9 +22,8 @@ $(document).ready(function() {
     $('#gen-new-key').hide();
     $('#rsa-form').show();
     return false;
-  })
-
-});
+  });
+}
 
 function genRSA(passphrase) {
   // gen RSA key and public key with passphrase
@@ -59,7 +65,6 @@ function saveKeyToDB(key) {
 
 // TODO: cryptico library creates an RSA object. Need to ID how to convert that object into a private key string 
 function downloadKey(key) {
-  console.log('key: ', key)
   var blob = new Blob([JSON.stringify(key)], {type : 'octet/stream'});
   blob = window.URL.createObjectURL(blob);
   const a = document.createElement('a');
@@ -68,3 +73,37 @@ function downloadKey(key) {
   a.click()
   window.URL.revokeObjectURL(blob)
 }
+
+
+
+
+/* -------- File Encryption Service --------- */
+
+function initFileEncryptionService() {
+  // TODO: UI!
+}
+
+function readFile(files, publicKey) {
+  for (let file in files) {
+    let reader = new FileReader()
+    reader.onload = () => {
+      const fileContent = JSON.parse(reader.result);
+      // encrypt file content
+      const encryptedContent = cryptico.encrypt(fileContent, publicKey);
+      
+      // TODO: overwrite original with encryptedContent
+
+
+      // make file available for download
+      handleFileDownloadAvailable();
+    }
+    if (typeof files[file] !== 'object') { continue; }
+    reader.readAsText(files[file])
+  }
+}
+
+function handleFileDownloadAvailable() {
+  // TODO: UI!
+}
+
+
