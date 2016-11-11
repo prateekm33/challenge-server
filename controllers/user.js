3a1d7e5cd7ab7a1eb8838bcf3ad99751b4214280
Prateek Madapurmath <prateekm33@gmail.com>
2016-11-10 16:58:25 -0800

    updates to user controller for handling save event

--- controllers/user.js
+++ controllers/user.js
@@ -376,8 +376,9 @@ exports.saveRSAkey = (req, res) => {
   const key = req.body.key;
 
   // TODO: Fix this! Need to ID why req.user not defined if session has been created -- connected to CSRF token
+  req.user = req.user || {};
 
-  User.findOne({ email: req.body.email }, (err, user) => {
+  User.findById(req.user.id, (err, user) => {
     if (err || !user) {
       console.log('User not found')
       return;
